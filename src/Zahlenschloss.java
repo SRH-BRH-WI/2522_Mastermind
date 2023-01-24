822c27a4caf57f0a48f49164fbfa6c82bce72440
Dr. Dillinger <peter.dillinger@srh.de>
2023-01-24 13:51:27 +0100

    Initial commit

---
+++ src/Zahlenschloss.java
@@ -0,0 +1,18 @@
+public class Zahlenschloss {
+
+
+    public static void main(String[] args) {
+
+        int geheimeZahl = 473;
+
+        // TODO
+        // Der Anwender soll eine Zahl eingeben,
+        // wenn die Zahl der geheimen Zahl entspricht,
+        // hat er gewonnen, ansonsten soll er weiter
+        // raten. Falls er keine Lust mehr zu raten hat,
+        // kann er das Programm mit der Eingabe von 0
+        // beenden.
+
+    }
+
+}
